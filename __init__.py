132645b82489e372322ceea4365ebfad7189b498
vedant-ydv <vedantyadav273115@gmail.com>
2025-04-27 03:20:10 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+Package initialization file.
+Makes this directory a Python package.
+"""
+
+# Version information
+__version__ = '0.1.0'
+
+# You can specify which symbols to export when using "from package import *"
+__all__ = []
